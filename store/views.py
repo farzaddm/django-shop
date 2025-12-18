af5f243220ed147aca40cad4ad028d95b85ff960
farzaddm <farzad.d.m84@gmail.com>
2025-12-18 13:56:30 +0330

    add permission for each endpoint

--- store/views.py
+++ store/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.mixins import (
     CreateModelMixin,
@@ -100,3 +102,21 @@ class CartItemViewSet(ModelViewSet):
 class CustomerViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
   queryset = Customer.objects.all()
   serializer_class = CustomerSerializer
+  permission_classes = [IsAuthenticated]
+  
+  def get_permissions(self):
+    if self.request.method == 'GET':
+      return [AllowAny()]
+    return [IsAuthenticated()]
+
+  @action(detail=False, methods=['GET', 'PUT'])
+  def me(self, request):
+    (customer, created) = Customer.objects.get_or_create(user_id=request.user.id)
+    if request.method == 'GET':
+      serializer = CustomerSerializer(customer)
+      return Response(serializer.data)
+    elif request.method == 'PUT':
+      serializer = CustomerSerializer(customer, data=request.data)
+      serializer.is_valid(raise_exception=True)
+      serializer.save()
+      return Response(serializer.data)
